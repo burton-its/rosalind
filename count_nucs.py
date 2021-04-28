4a80fb6ffe64f345b180874f7476789326ae1ba5
burton-its <54475896+burton-its@users.noreply.github.com>
2021-04-28 11:05:08 -0400

    rosalind problem set

---
+++ count_nucs.py
@@ -0,0 +1,9 @@
+nucs = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+
+a = nucs.count("A")
+c = nucs.count("C")
+g = nucs.count("G")
+t = nucs.count("T")
+
+
+print(a,c,g,t)
---
+++ dna_rna.py
@@ -0,0 +1,6 @@
+dna1 = "GATGGAACTTGACTACGTAAATT"
+
+
+rna1 = dna1.replace("T","U")
+
+print(rna1)
